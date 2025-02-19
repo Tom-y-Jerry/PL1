0d9f2cd74f0483ec9af6bf0bb8f3505afddac197
carlota <a@a.com>
2025-02-19 15:50:27 +0000

    Implement the class Calculator

--- src/main/java/es/ulpgc/dacd/main.java
+++ src/main/java/es/ulpgc/dacd/main.java
@@ -1,9 +1,9 @@
 package es.ulpgc.dacd;
 
 public class main {
-    public static void printres(String[] args) {
+    public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println("Suma: "+ calc.calculate("add", 5, 3));
-        System.out.println("Resta: "+ calc.calculate("subtract", 5, 3));
+        System.out.println("Suma: " + calc.calculate("add", 5, 3));
+        System.out.println("Resta: " + calc.calculate("subtract", 5, 3));
     }
 }
